4bde8534dbd6cb85109accde992e05d37ca0cf9a
Bia Coelho <biacoelho@pm.me>
2021-06-22 00:27:45 -0300

    aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
+import "reflect-metadata";
 import express from 'express';
+
+import { router } from './routes';
+
+import './database';
 
 const app = express();
 
-app.get("/test", (request, response) => {
-  return response.send("Olá Bia Coelho");
-});
+app.use(express.json());
+app.use(router);
 
-app.post("/test-post", (request, response) => {
-  return response.send("Olá método POST");
-});
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
